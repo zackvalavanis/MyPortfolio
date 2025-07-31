af3342301748bcf7e7f3b56dc9767cf38d57dedb
Zack <168221270+Zval321@users.noreply.github.com>
2025-07-30 20:11:05 -0500

    added navbar to the top of a few pages

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -5,28 +5,30 @@ import { NavBar } from "./Navbar";
 
 export function Header() {
   return (
-    <div className='header-container'>
+    <div>
       <NavBar />
-      <div className='header-name-links-container'>
-        <div className='left-side-image-header-container'>
-          <img className='image-header-photo' src='./Valavanis_Zack_NBPW_Gradient_1x1.25.jpg' />
-        </div>
-        <div className='right-side-header'>
-          <div className='header-container-name'>
-            <h1><b>I'm Zack Valavanis, a web designer and software engineer.</b></h1>
+      <div className='header-container'>
+        <div className='header-name-links-container'>
+          <div className='left-side-image-header-container'>
+            <img className='image-header-photo' src='./Valavanis_Zack_NBPW_Gradient_1x1.25.jpg' />
           </div>
-          <div className='paragraph-header-container'>
-            <p>
-              Welcome to my website! Here, you can explore my journey in computer science, see my projects, and learn about my skills and passions.
-            </p>
+          <div className='right-side-header'>
+            <div className='header-container-name'>
+              <h1><b>I'm Zack Valavanis, a web designer and software engineer.</b></h1>
+            </div>
+            <div className='paragraph-header-container'>
+              <p>
+                Welcome to my website! Here, you can explore my journey in computer science, see my projects, and learn about my skills and passions.
+              </p>
+            </div>
+            <a
+              href='#about-me'
+              className='header-link'
+            >About Me</a>
           </div>
-          <a
-            href='#about-me'
-            className='header-link'
-          >About Me</a>
         </div>
-      </div>
-      <div>
+        <div>
+        </div>
       </div>
     </div>
   )
