cf57264297704070fbb1ed82eb64420b2fd1182a
Zack <168221270+Zval321@users.noreply.github.com>
2025-08-15 13:43:42 -0500

    reorginize the main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -44,6 +44,16 @@ export function Main() {
             </img>
           </div>
           <div className='about-me-text-section-1-container'>
+            <div className='goals-container'>
+              <h1 className='header-about-me-text-container'>2025 Goals:</h1>
+              <ul className='bullets-about-me-goals'>
+                <li>Enroll in a Post-Baccalaureate Program in CS as the first step toward a Master’s in Computer Science</li>
+                <li>Complete personal software projects</li>
+                <li>Apply for internships or relevant experience in tech</li>
+                <li>Expand knowledge in algorithms and data structures</li>
+              </ul>
+            </div>
+
             <div className='header-about-me-text-container'>
               <h1>
                 About Me:
@@ -53,16 +63,6 @@ export function Main() {
               <p>
                 Hello! My name is Zack Valavanis, and I’m passionate about continuing my journey into computer science and software engineering. I’m originally from the northside suburbs of Chicago, and in my free time, I enjoy playing guitar, going to concerts, traveling, and building side projects that challenge and inspire me.
               </p>
-            </div>
-
-            <div className='goals-container'>
-              <h1 className='header-about-me-text-container'>2025 Goals:</h1>
-              <ul className='bullets-about-me-goals'>
-                <li>Enroll in a Post-Baccalaureate Program in CS as the first step toward a Master’s in Computer Science</li>
-                <li>Complete personal software projects</li>
-                <li>Apply for internships or relevant experience in tech</li>
-                <li>Expand knowledge in algorithms and data structures</li>
-              </ul>
             </div>
 
             <div className='button-about-me-container'>
@@ -80,7 +80,7 @@ export function Main() {
           <div className='left-side-section-2-container'>
             <div id='experience' className='header-section-2'>
               <h1>
-                Experience:
+                Experience
               </h1>
             </div>
 
@@ -194,10 +194,82 @@ export function Main() {
 
 
           <div className='right-side-section-2-container'>
-            <h1>Education & Certifications</h1>
+            <img src='public/coding_image.jpg' />
           </div>
         </section>
       </div >
+
+      <div>
+        <section>
+          <div>
+            <h1 className='header-right-section-2'>Education & Certifications</h1>
+
+          </div>
+          <div className='accordian-container'>
+            <Accordion expanded={true}>
+              <AccordionSummary sx={{ cursor: 'default' }}>
+                <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+                  <Typography sx={{ fontSize: 18 }} fontWeight="bold">The University of Colorado Boulder</Typography>
+                  <Typography sx={{ fontSize: 16 }} variant="body2" color="text.secondary">– Bachelor of Science in Business Administration, Emphasis in Finance</Typography>
+                </Box>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List>
+                  <ListItem>
+                    <ListItemIcon sx={{ minWidth: 20 }}>
+                      <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Built frontend using React and Material UI" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon sx={{ minWidth: 20 }}>
+                      <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Integrated backend with Firebase and REST APIs" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon sx={{ minWidth: 20 }}>
+                      <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Led a team of 3 developers" />
+                  </ListItem>
+                </List>
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion expanded={true}>
+              <AccordionSummary sx={{ cursor: 'default' }}>
+                <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+                  <Typography sx={{ fontSize: 18 }} fontWeight="bold">University of Wesminster - London, UK</Typography>
+                  <Typography sx={{ fontSize: 16 }} variant="body2" color="text.secondary">– Study Abroad</Typography>
+                </Box>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List>
+                  <ListItem>
+                    <ListItemIcon sx={{ minWidth: 20 }}>
+                      <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Built frontend using React and Material UI" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon sx={{ minWidth: 20 }}>
+                      <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Integrated backend with Firebase and REST APIs" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon sx={{ minWidth: 20 }}>
+                      <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Led a team of 3 developers" />
+                  </ListItem>
+                </List>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        </section>
+      </div>
 
       <div className='section-3-container'>
         <section id='skills'>
---
+++ src/pages/Main/temp.tsx
@@ -0,0 +1,67 @@
+{/* <h1 className='header-right-section-2'>Education & Certifications</h1>
+
+<div className='accordian-container'>
+  <Accordion expanded={true}>
+    <AccordionSummary sx={{ cursor: 'default' }}>
+      <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+        <Typography sx={{ fontSize: 18 }} fontWeight="bold">The University of Colorado Boulder</Typography>
+        <Typography sx={{ fontSize: 16 }} variant="body2" color="text.secondary">– Bachelor of Science in Business Administration, Emphasis in Finance</Typography>
+      </Box>
+    </AccordionSummary>
+    <AccordionDetails>
+      <List>
+        <ListItem>
+          <ListItemIcon sx={{ minWidth: 20 }}>
+            <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+          </ListItemIcon>
+          <ListItemText primary="Built frontend using React and Material UI" />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon sx={{ minWidth: 20 }}>
+            <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+          </ListItemIcon>
+          <ListItemText primary="Integrated backend with Firebase and REST APIs" />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon sx={{ minWidth: 20 }}>
+            <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+          </ListItemIcon>
+          <ListItemText primary="Led a team of 3 developers" />
+        </ListItem>
+      </List>
+    </AccordionDetails>
+  </Accordion>
+
+  <Accordion expanded={true}>
+    <AccordionSummary sx={{ cursor: 'default' }}>
+      <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+        <Typography sx={{ fontSize: 18 }} fontWeight="bold">University of Wesminster - London, UK</Typography>
+        <Typography sx={{ fontSize: 16 }} variant="body2" color="text.secondary">– Study Abroad</Typography>
+      </Box>
+    </AccordionSummary>
+    <AccordionDetails>
+      <List>
+        <ListItem>
+          <ListItemIcon sx={{ minWidth: 20 }}>
+            <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+          </ListItemIcon>
+          <ListItemText primary="Built frontend using React and Material UI" />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon sx={{ minWidth: 20 }}>
+            <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+          </ListItemIcon>
+          <ListItemText primary="Integrated backend with Firebase and REST APIs" />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon sx={{ minWidth: 20 }}>
+            <FiberManualRecordIcon sx={{ fontSize: 8 }} />
+          </ListItemIcon>
+          <ListItemText primary="Led a team of 3 developers" />
+        </ListItem>
+      </List>
+    </AccordionDetails>
+  </Accordion>
+</div>
+</div>
+</section> */}
