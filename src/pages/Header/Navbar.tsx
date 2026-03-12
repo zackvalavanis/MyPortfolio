b1e26736ab4fda6cf170603d743d748e0dd29e6b
Zack <168221270+Zval321@users.noreply.github.com>
2026-03-12 12:49:07 -0500

    fix navbar css casing

--- src/pages/Header/Navbar.tsx
+++ src/pages/Header/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./Navbar.css";
+import './NavBar.css'
 
 const NAV_ITEMS = [
   { label: "About", id: "about-me" },
