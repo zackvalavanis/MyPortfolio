c0111be2175bf76e664210252586c589a77f2ba1
Zack <168221270+Zval321@users.noreply.github.com>
2026-04-17 16:03:39 -0500

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/appRoutes.tsx'
 import './App.css'
 import '../src/style/style.css'
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   
