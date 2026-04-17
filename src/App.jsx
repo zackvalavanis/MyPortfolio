ebbd5b84c0fbe07724bd317d90750d9f2e3d00f7
Zack <168221270+Zval321@users.noreply.github.com>
2026-04-17 16:01:51 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/appRoutes.tsx'
 import './App.css'
 import '../src/style/style.css'
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   
   return (
-   <RouterProvider router={router}/>
+  <>
+    <RouterProvider router={router}/>
+    <Analytics />
+  </>
   )
 }
 
